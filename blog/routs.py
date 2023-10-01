de9f516bea1ebe3bd6b610a632073f4b9b7aada5
Okeomasilachi <okeomasilachi@gmail.com>
2023-10-01 16:06:57 +0100

    update rout

--- blog/routs.py
+++ blog/routs.py
@@ -1,5 +1,8 @@
 from flask import render_template, url_for, flash, redirect, request
 from blog import app, db, bc
+import secrets
+import os
+from PIL import Image
 from blog.form import Regfrom, Loginfrom, UpdateAccountFrom
 from blog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
@@ -73,10 +76,36 @@ def logout():
     logout_user()
     return redirect(url_for("home"))
 
-@app.route("/account",)
+
+def save_pic(form_pic):
+    rand_hex = secrets.token_hex(8)
+    _, f_ext = os.path.spliext(form_pic.filename)
+    new_file_name = rand_hex + f_ext
+    path = os.path.join(app.root_path, "static/dpics", new_file_name)
+    
+    size = (125, 125)
+    img = Image.open(from_pic)
+    img.thumbnail(size)
+    img.save(path)
+    return path
+
+
+@app.route("/account", methods=["GET", "POST"])
 @login_required
 def account():
-    form = UpdateAccpuntFrom
+    form = UpdateAccountFrom()
+    if form.validate_on_submit():
+        if form.picture.data:
+            pic = save_pic(form.picture.data)
+            current_user.image_file = pic
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Account Info Updated", "success")
+        return redirect(url_for("account"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     image_file = url_for("static", filename="dpics/" + current_user.image_file)
     return render_template("account.html", title="Account",
                            image_file=image_file, form=form)
